50e718e12f38e10f9e514a5ec3b13ac95bb45153
hddn <hddnsmile@gmail.com>
2015-08-29 20:39:14 +0300

    add dinamic student table

--- students/views.py
+++ students/views.py
@@ -1,9 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Ентоні',
+         'last_name': u'Хопкінс',
+         'ticket': 42,
+         'image': 'img/ah.jpg'},
+         {'id': 2,
+         'first_name': u'Скарлетт',
+         'last_name': u'Йоханссон',
+         'ticket': 11,
+         'image': 'img/sy.jpg'},
+         {'id': 3,
+         'first_name': u'Майкл',
+         'last_name': u'Джордан',
+         'ticket': 23,
+         'image': 'img/mj.jpg'},
+         )
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
