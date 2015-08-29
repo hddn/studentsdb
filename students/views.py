28d696e5daad7df491b4b99eea8d4feea9b19f30
hddn <hddnsmile@gmail.com>
2015-08-29 19:50:07 +0300

    add dinamic urls

--- students/views.py
+++ students/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+
+def students_add(request):
+    return HttpResponse('<h2>Add student</h2>')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h2>Edit student %s</h2>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h2>Delete student %s</h2>' % sid)
+
+
+
+def groups_list(request):
+    return HttpResponse('<h2>Groups List</h2>')
+
+
+def groups_add(request):
+    return HttpResponse('<h2>Add group</h2>')
+
+
+def groups_edit(request, gid):
+    return HttpResponse('<h2>Edit group %s</h2>' % gid)
+
+
+def groups_delete(request, gid):
+    return HttpResponse('<h2>Delete group %s</h2>' % gid)
+
