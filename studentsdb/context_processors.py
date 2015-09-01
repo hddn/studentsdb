156d0a6bd0da9e7a07a0087fc42a923d3e5d398f
hddn <hddnsmile@gmail.com>
2015-09-01 19:06:11 +0300

    add custom context proc

---
+++ studentsdb/context_processors.py
@@ -0,0 +1,4 @@
+from .settings import PORTAL_URL
+
+def student_proc(request):
+    return {'PORTAL_URL': PORTAL_URL}
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
 
@@ -101,3 +102,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
