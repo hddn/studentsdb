e0131bbe9fd7f2291ce05434ef138a8afd35def9
hddn <hddnsmile@gmail.com>
2015-09-02 23:49:26 +0300

    connect database with students page

--- students/views/students.py
+++ students/views/students.py
@@ -2,29 +2,29 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Student
+
 # Create your views here.
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Ентоні',
-         'last_name': u'Хопкінс',
-         'ticket': 42,
-         'group': 'ЗСІ-11',   
-         'image': 'img/ah.jpg'},
-         {'id': 2,
-         'first_name': u'Скарлетт',
-         'last_name': u'Йоханссон',
-         'ticket': 11,
-         'group': 'ЗСІ-12',
-         'image': 'img/sy.jpg'},
-         {'id': 3,
-         'first_name': u'Майкл',
-         'last_name': u'Джордан',
-         'ticket': 23,
-         'group': 'ЗСІ-13',
-         'image': 'img/mj.jpg'},
-         )
+    students = Student.objects.all()
+
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        students = paginator.page(1)
+    except EmptyPage:
+        students = paginator.page(paginator.num_pages)
+    
     return render(request, 'students/students_list.html', {'students': students})
 
 
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
     url(r'^$', 'students.views.students.students_list', name='home'),
@@ -37,3 +38,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     )
+
+if DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': MEDIA_ROOT}))
