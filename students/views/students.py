88668a2b724121d2b8783eb3ae9f16b6a5f1da5d
hddn <hddnsmile@gmail.com>
2015-09-10 20:46:49 +0300

    make add_student form

--- students/views/students.py
+++ students/views/students.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models.student import Student
+from ..models.group import Group
 
 # Create your views here.
 
@@ -29,7 +31,32 @@ def students_list(request):
 
 
 def students_add(request):
-    return HttpResponse('<h2>Add student</h2>')
+    if request.method == 'POST':
+        if request.POST.get('add_button') is not None:
+            errors = {}
+            if not errors:
+                student = Student(
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    middle_name=request.POST['middle_name'],
+                    birthday=request.POST['birthday'],
+                    ticket=request.POST['ticket'],
+                    student_group=Group.objects.get(pk=request.POST['student_group']),
+                    photo=request.FILES['photo'],)
+                student.save()
+
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),
+                     'errors': errors})
+
+        elif request.POST.get('cancel_button') is not None:
+            return HttpResponseRedirect(reverse('home'))
+
+    else:
+        return render(request, 'students/students_add.html',
+            {'groups': Group.objects.all().order_by('title')})
 
 
 def students_edit(request, sid):
