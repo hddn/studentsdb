8fec1892ce7a30fefcc80f78402303cf91a0e065
hddn <hddnsmile@gmail.com>
2015-09-10 22:54:04 +0300

    make working alert and error messages

--- students/views/students.py
+++ students/views/students.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models.student import Student
 from ..models.group import Group
+from datetime import datetime
 
 # Create your views here.
 
@@ -34,25 +35,68 @@ def students_add(request):
     if request.method == 'POST':
         if request.POST.get('add_button') is not None:
             errors = {}
+            data = {'middle_name': request.POST.get('middle_name'),
+                    'notes': request.POST.get('notes')}
+
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u'Ім’я є обов’язковим'
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u'Прізвище є обов’язковим'
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u'Введіть дату народження'
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = u'Введіть коректну дату (напр. 1988-11-30)'
+                else:
+                    data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u'Введіть номер білета'
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = u'Оберіть групу'
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = u'Оберіть коректну групу'
+                else:
+                    data['student_group'] = Group.objects.get(pk=student_group)
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
+
             if not errors:
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],)
+                student = Student(**data)
                 student.save()
 
-                return HttpResponseRedirect(reverse('home'))
+                return HttpResponseRedirect(
+                    u'%s?status_message=Студента успішно додано!' %
+                    reverse('home'))
             else:
                 return render(request, 'students/students_add.html',
                     {'groups': Group.objects.all().order_by('title'),
                      'errors': errors})
 
         elif request.POST.get('cancel_button') is not None:
-            return HttpResponseRedirect(reverse('home'))
+            return HttpResponseRedirect(
+                u'%s?status_message=Додавання студента скасовано!' %
+                reverse('home'))
 
     else:
         return render(request, 'students/students_add.html',
