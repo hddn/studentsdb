62aedc5718d551cc1a1139253a98ba5ee84412ac
hddn <hddnsmile@gmail.com>
2017-04-04 15:43:22 +0300

    refactor journal model

--- students/models/monthjournal.py
+++ students/models/monthjournal.py
@@ -14,38 +14,11 @@ class MonthJournal(models.Model):
     date = models.DateField(
         verbose_name=_('Date'),
         blank=False)
-    # TODO: refactor this shit
-    present_day1 = models.BooleanField(default=False)
-    present_day2 = models.BooleanField(default=False)
-    present_day3 = models.BooleanField(default=False)
-    present_day4 = models.BooleanField(default=False)
-    present_day5 = models.BooleanField(default=False)
-    present_day6 = models.BooleanField(default=False)
-    present_day7 = models.BooleanField(default=False)
-    present_day8 = models.BooleanField(default=False)
-    present_day9 = models.BooleanField(default=False)
-    present_day10 = models.BooleanField(default=False)
-    present_day11 = models.BooleanField(default=False)
-    present_day12 = models.BooleanField(default=False)
-    present_day13 = models.BooleanField(default=False)
-    present_day14 = models.BooleanField(default=False)
-    present_day15 = models.BooleanField(default=False)
-    present_day16 = models.BooleanField(default=False)
-    present_day17 = models.BooleanField(default=False)
-    present_day18 = models.BooleanField(default=False)
-    present_day19 = models.BooleanField(default=False)
-    present_day20 = models.BooleanField(default=False)
-    present_day21 = models.BooleanField(default=False)
-    present_day22 = models.BooleanField(default=False)
-    present_day23 = models.BooleanField(default=False)
-    present_day24 = models.BooleanField(default=False)
-    present_day25 = models.BooleanField(default=False)
-    present_day26 = models.BooleanField(default=False)
-    present_day27 = models.BooleanField(default=False)
-    present_day28 = models.BooleanField(default=False)
-    present_day29 = models.BooleanField(default=False)
-    present_day30 = models.BooleanField(default=False)
-    present_day31 = models.BooleanField(default=False)
 
     def __str__(self):
         return '{}: {:d}, {:d}'.format(self.student.last_name, self.date.month, self.date.year)
+
+
+for i in range(1, 32):
+    MonthJournal.add_to_class('present_day{}'.format(i), models.BooleanField(default=False))
+
